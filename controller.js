cef1939b6c1d9cab2bc4532f8486eef74294cb21
Satish Mishra <sattasaphire@gmail.com>
2015-06-12 11:28:39 +0530

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,12 +1,39 @@
 var app = angular.module('myApp', []);
 
 app.controller('DefaultController', function ($scope) {
-    $scope.name = 'Geek';
     
-    $scope.searchCalled = function(){
-         $scope.searchActive = true;
+    $scope.study = ['MBA','GATE','AIPMT','ENGG','AIEEE','IIT','NET','PUBLIC','DEFENCE','OTHER'];
+    $scope.questionlist = ['CAT','XAT','GMAT','CMAT','IIFT','CET','MAT','OTHER'];
+    $scope.percentlist = ['40-50','51-60','61-70','71-80','81-90','91-99'];
+    $scope.graduationlist = ['ENGG','BCOM','BBA','BSC','BA','MBBS','BJMC','BCA'];
+    $scope.hours= ['one','two','three','four','five','six','seven','eight'];
+    
+    $scope.fieldSelected = true;
+    
+    $scope.studyClicked = function(){
+        $scope.fieldSelected = false;
+        $scope.examSelected = true;
     }
-    $scope.searchDeactive = function(){
-         $scope.searchActive = false;   
+    $scope.courseClicked = function(courses){
+        $scope.examSelected = false;
+        $scope.courseSelected = true;
+        console.log(courses);
+    }
+    $scope.testClicked = function(test){
+        console.log(test);
+        $scope.courseSelected = false;
+        $scope.testSelected = true;
+    }
+    $scope.tenthClicked = function(percent){
+        $scope.testSelected = false;
+        $scope.tenthSelected = true;
+    }
+    $scope.twelthClicked = function(){
+        $scope.tenthSelected = false;
+        $scope.twelthSelected = true;
+    }
+    $scope.graduationClicked = function(){
+        $scope.twelthSelected = false;
+        $scope.graduationSelected = true;
     }
 });
